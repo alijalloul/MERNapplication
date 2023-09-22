e6f41f2e1a7f06e1d8fa17dad4cd1430cee59105
unknown <ali.z.jalloul@gmail.com>
2023-09-22 18:32:02 +0300

    pinging both client and server every 13min

--- backend/index.js
+++ backend/index.js
@@ -4,269 +4,284 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import fs from "fs";
-import path from "path";    
-
+import path from "path";
+import https from "https";
 import auth from "./middleware/middleware.js";
 import postDB from "./schema/postSchema.js";
 import userDB from "./schema/userSchema.js";
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit:'50mb'}));
+app.use(express.json({ limit: "50mb" }));
+
+if (process.env.PORT) {
+  console.log("PORT EXISTS");
+  function pingWebsite() {
+    https
+      .get("https://recollections-server.onrender.com", (res) => {
+        console.log("Website pinged successfully");
+      })
+      .on("error", (err) => {
+        console.error("Error while pinging website:", err);
+      });
+  }
+
+  // Ping website every 14 minutes (840000 milliseconds)
+  setInterval(pingWebsite, 840000);
+}
 
 const imagesFolder = "./postImages";
-const atlasURL = "mongodb+srv://reactjsdatabase:12345@cluster0.1djt5oa.mongodb.net/test?retryWrites=true&w=majority";    
+const atlasURL = "mongodb+srv://reactjsdatabase:12345@cluster0.1djt5oa.mongodb.net/test?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(atlasURL)
-    .then(() => app.listen(PORT, () => console.log(`Successfully connected to port ${PORT}`)))
-    .catch(error => console.log("There was an error: ", error));
-
+mongoose
+  .connect(atlasURL)
+  .then(() => app.listen(PORT, () => console.log(`Successfully connected to port ${PORT}`)))
+  .catch((error) => console.log("There was an error: ", error));
 
 //app.listen(PORT, () => console.log(`Successfully connected to port ${PORT}`));
 app.get("/", async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "true");
-    res.send("SERVER IS RUNNIGN");
+  res.setHeader("Access-Control-Allow-Origin", "true");
+  res.send("SERVER IS RUNNIGN");
 });
 app.get("/posts", async (req, res) => {
-    const { page } = req.query;
+  const { page } = req.query;
 
-    try {
-        const LIMIT = 8;
-        const startIndex = (Number(page) - 1) * LIMIT;
-        const totalPosts = await postDB.countDocuments({});
+  try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT;
+    const totalPosts = await postDB.countDocuments({});
 
-        const posts = await postDB.find().sort({ _id: -1}).limit(LIMIT).skip(startIndex);
+    const posts = await postDB.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
 
-        posts.map(post => {
-            const imagePath = path.join(imagesFolder, post.file);
+    posts.map((post) => {
+      const imagePath = path.join(imagesFolder, post.file);
 
-            var base64Data;
-            if(fs.existsSync(imagePath)){
-                base64Data = fs.readFileSync(imagePath, { encoding: 'base64' });
-            }else {
-                base64Data = fs.readFileSync('./postImages/notfound.jpeg').toString("base64");
-            }
+      var base64Data;
+      if (fs.existsSync(imagePath)) {
+        base64Data = fs.readFileSync(imagePath, { encoding: "base64" });
+      } else {
+        base64Data = fs.readFileSync("./postImages/notfound.jpeg").toString("base64");
+      }
 
+      post.file = "data:image/png;base64," + base64Data;
+    });
 
-
-            post.file = "data:image/png;base64," + base64Data;  
-        })
-
-        
-        res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(totalPosts/LIMIT)});
-    } catch (error) {
-        res.send("There was an error: ", error);
-    }
+    res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(totalPosts / LIMIT) });
+  } catch (error) {
+    res.send("There was an error: ", error);
+  }
 });
 app.get("/posts/search", async (req, res) => {
-    const { searchQuery, tags, page } = req.query;
+  const { searchQuery, tags, page } = req.query;
 
-    try {
-        const LIMIT = 8;
-        const startIndex = (Number(page) - 1) * LIMIT;
+  try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT;
 
-        const title = new RegExp(searchQuery, 'i'); 
-        const totalPosts = await postDB.countDocuments({ $or: [{ title }, { tags: { $in: tags.split(",") } }] });
-        
-        const posts = await postDB.find({ $or: [{ title }, { tags: { $in: tags.split(",") } }] }).sort({ _id: -1}).limit(LIMIT).skip(startIndex);
+    const title = new RegExp(searchQuery, "i");
+    const totalPosts = await postDB.countDocuments({ $or: [{ title }, { tags: { $in: tags.split(",") } }] });
 
-        posts.map(post => {
-            const imagePath = path.join(imagesFolder, post.file);
+    const posts = await postDB
+      .find({ $or: [{ title }, { tags: { $in: tags.split(",") } }] })
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
 
-            var base64Data;
-            if(fs.existsSync(imagePath)){
-                base64Data = fs.readFileSync(imagePath, { encoding: 'base64' });
-            }else {
-                base64Data = fs.readFileSync('./postImages/notfound.jpeg').toString("base64");
-            }
+    posts.map((post) => {
+      const imagePath = path.join(imagesFolder, post.file);
 
+      var base64Data;
+      if (fs.existsSync(imagePath)) {
+        base64Data = fs.readFileSync(imagePath, { encoding: "base64" });
+      } else {
+        base64Data = fs.readFileSync("./postImages/notfound.jpeg").toString("base64");
+      }
 
+      post.file = "data:image/png;base64," + base64Data;
+    });
 
-            post.file = "data:image/png;base64," + base64Data;  
-        })
-
-        res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(totalPosts/LIMIT)});
-    } catch (error) {
-        res.status(409).json( { message: error.message } );
-    }
+    res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(totalPosts / LIMIT) });
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 });
 app.get("/posts/:id", async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const posts = await postDB.findById(id);
+  try {
+    const posts = await postDB.findById(id);
 
-        const imagePath = path.join(imagesFolder, posts.file);
+    const imagePath = path.join(imagesFolder, posts.file);
 
-        var base64Data;
-        if(fs.existsSync(imagePath)){
-            base64Data = fs.readFileSync(imagePath, { encoding: 'base64' });
-        }else {
-            base64Data = fs.readFileSync('./postImages/notfound.jpeg').toString("base64");
-        }
-
-
-
-        posts.file = "data:image/png;base64," + base64Data;  
-
-        res.json(posts);
-    } catch (error) {
-        res.send("There was an error: ", error);
+    var base64Data;
+    if (fs.existsSync(imagePath)) {
+      base64Data = fs.readFileSync(imagePath, { encoding: "base64" });
+    } else {
+      base64Data = fs.readFileSync("./postImages/notfound.jpeg").toString("base64");
     }
+
+    posts.file = "data:image/png;base64," + base64Data;
+
+    res.json(posts);
+  } catch (error) {
+    res.send("There was an error: ", error);
+  }
 });
 app.post("/posts", async (req, res) => {
-    const body = req.body;
-    const base64Image = body.file;
+  const body = req.body;
+  const base64Image = body.file;
 
-    // Extract image type and base64 data
-    const fileExtension = base64Image.split(';')[0].split('/')[1];
-    const buffer = Buffer.from(base64Image.split(',')[1], 'base64');
-    
-    const filename = `image-${body.creatorID}-${Date.now()}.${fileExtension}`;
-    fs.writeFileSync(path.join(imagesFolder, filename), buffer);
+  // Extract image type and base64 data
+  const fileExtension = base64Image.split(";")[0].split("/")[1];
+  const buffer = Buffer.from(base64Image.split(",")[1], "base64");
 
-    body.file = filename;
+  const filename = `image-${body.creatorID}-${Date.now()}.${fileExtension}`;
+  fs.writeFileSync(path.join(imagesFolder, filename), buffer);
 
-    console.log("body.file changed: ", body);
+  body.file = filename;
 
-    const newPost = new postDB(body);
+  console.log("body.file changed: ", body);
 
-    try {
-        await newPost.save();
+  const newPost = new postDB(body);
 
-        res.json({
-            status: "success",
+  try {
+    await newPost.save();
 
-            _id: newPost._id,
-            title: body.title,
-            message: body.message,
-            name: body.name,
-            tags: body.tags,
-            file: body.file,
-            likes: body.likes,
-            createdAt: body.createdAt,
-        });
-    } catch (error) {
-        res.status(409).json( { message: error.message } );
-    }
+    res.json({
+      status: "success",
+
+      _id: newPost._id,
+      title: body.title,
+      message: body.message,
+      name: body.name,
+      tags: body.tags,
+      file: body.file,
+      likes: body.likes,
+      createdAt: body.createdAt,
+    });
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 });
 
-app.patch('/:id', auth, async (req, res) => {
-    const { id } = req.params;
-    const post = req.body;
+app.patch("/:id", auth, async (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
 
-    const base64Image = post.file;
+  const base64Image = post.file;
 
-    // Extract image type and base64 data
-    const fileExtension = base64Image.split(';')[0].split('/')[1];
-    const buffer = Buffer.from(base64Image.split(',')[1], 'base64');
-    
-    const filename = `image-${post.creatorID}-${Date.now()}.${fileExtension}`;
-    fs.writeFileSync(path.join(imagesFolder, filename), buffer);
+  // Extract image type and base64 data
+  const fileExtension = base64Image.split(";")[0].split("/")[1];
+  const buffer = Buffer.from(base64Image.split(",")[1], "base64");
 
-    post.file = filename;    
+  const filename = `image-${post.creatorID}-${Date.now()}.${fileExtension}`;
+  fs.writeFileSync(path.join(imagesFolder, filename), buffer);
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).send('No post with that id');
-    }
+  post.file = filename;
 
-    const updatedPost = await postDB.findByIdAndUpdate(id, post, { new: true});
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("No post with that id");
+  }
 
-    res.json(updatedPost);
+  const updatedPost = await postDB.findByIdAndUpdate(id, post, { new: true });
+
+  res.json(updatedPost);
 });
 
-app.patch('/:id/likePost', auth, async (req, res) => {
-    const { id } = req.params;
+app.patch("/:id/likePost", auth, async (req, res) => {
+  const { id } = req.params;
 
-    if(!req.userId) return res.json({ message: "unauthenticated" });
+  if (!req.userId) return res.json({ message: "unauthenticated" });
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).send('No post with that id');
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("No post with that id");
+  }
 
-    const post = await postDB.findById(id);
-    const index = post.likes.findIndex((id) => id === String(req.userId));
+  const post = await postDB.findById(id);
+  const index = post.likes.findIndex((id) => id === String(req.userId));
 
-    if(index === -1){
-        post.likes.push(req.userId);
-    }else{
-        post.likes = post.likes.filter(id => id !== String(req.userId));
-    }
-    const updatedPost = await postDB.findByIdAndUpdate(id, { likes: post.likes }, { new: true});
+  if (index === -1) {
+    post.likes.push(req.userId);
+  } else {
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+  const updatedPost = await postDB.findByIdAndUpdate(id, { likes: post.likes }, { new: true });
 
-    res.json(updatedPost.likes);
+  res.json(updatedPost.likes);
 });
 
-app.post('/:id/postComment', auth, async (req, res) => {
-    const { id } = req.params;
-    const comment = req.body.commentInfo;
-    console.log(comment);
+app.post("/:id/postComment", auth, async (req, res) => {
+  const { id } = req.params;
+  const comment = req.body.commentInfo;
+  console.log(comment);
 
-    if(!req.userId) return res.json({ message: "unauthenticated" });
+  if (!req.userId) return res.json({ message: "unauthenticated" });
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).send('No post with that id');
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("No post with that id");
+  }
 
-    const post = await postDB.findById(id);
-    
-    post.comments.push(comment);
+  const post = await postDB.findById(id);
 
-    const updatedPost = await postDB.findByIdAndUpdate(id, { comments: post.comments }, { new: true});
+  post.comments.push(comment);
 
-    res.json(updatedPost.comments);
+  const updatedPost = await postDB.findByIdAndUpdate(id, { comments: post.comments }, { new: true });
+
+  res.json(updatedPost.comments);
 });
 
-app.delete('/:id', async (req, res) => {
-    const { id } = req.params;
+app.delete("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).send('No post with that id');
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("No post with that id");
+  }
 
-    await postDB.findByIdAndRemove(id);
+  await postDB.findByIdAndRemove(id);
 
-    res.json({ message: 'Post deleted successfully!'});
+  res.json({ message: "Post deleted successfully!" });
 });
 
 app.post("/users/login", async (req, res) => {
-    const { email, password } = req.body;
-     
-    try{
-        const existingUser = await userDB.findOne({ email });
+  const { email, password } = req.body;
 
-        if(!existingUser) return res.status(404).json({ message: "User doesn't exist"});
+  try {
+    const existingUser = await userDB.findOne({ email });
 
-        const validPassword = await bcrypt.compare(password, existingUser.password);
+    if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
 
-        if(!validPassword) return res.status(400).json({ message: "Invalid password"});
+    const validPassword = await bcrypt.compare(password, existingUser.password);
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser._id}, "sk", { expiresIn: "1hr"});
-        
-        res.status(200).json({ result: existingUser, token});
-    }catch(error){
-        res.status(500).json({message: error.message});
-    }
-}); 
+    if (!validPassword) return res.status(400).json({ message: "Invalid password" });
+
+    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, "sk", { expiresIn: "1hr" });
+
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 app.post("/users/signup", async (req, res) => {
-    const { firstName, lastName, email, password, repassword} = req.body;
+  const { firstName, lastName, email, password, repassword } = req.body;
 
-    try{
-        const existingUser = await userDB.findOne({ email });
+  try {
+    const existingUser = await userDB.findOne({ email });
 
-        if(existingUser) {console.log("same User");return res.status(400).json({ message: "Account already exists." })};
-
-        if(password !== repassword) return res.status(400).json({ message: "Passwords do not match." });
-
-        const hashedPass = await bcrypt.hash(password, 12);
-
-        const result = await userDB.create({ email, password: hashedPass, name: `${firstName} ${lastName}`});
-        const token = jwt.sign({ email: result.email, id: result._id}, "sk", { expiresIn: "1hr"});
-
-        res.status(200).json({ result, token });
-
-    }catch (error){
-        res.status(500).json({message: error.message});
+    if (existingUser) {
+      console.log("same User");
+      return res.status(400).json({ message: "Account already exists." });
     }
+
+    if (password !== repassword) return res.status(400).json({ message: "Passwords do not match." });
+
+    const hashedPass = await bcrypt.hash(password, 12);
+
+    const result = await userDB.create({ email, password: hashedPass, name: `${firstName} ${lastName}` });
+    const token = jwt.sign({ email: result.email, id: result._id }, "sk", { expiresIn: "1hr" });
+
+    res.status(200).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,12 +1,13 @@
-import path from 'path';
-import fs from 'fs';
-import express from 'express';
-import React from 'react';
-import * as dotenv from "dotenv";
-import ReactDOMServer from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom/server';
-import { Provider } from 'react-redux';
 import { configureStore } from "@reduxjs/toolkit";
+import * as dotenv from "dotenv";
+import express from "express";
+import fs from "fs";
+import https from "https";
+import path from "path";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import { Provider } from "react-redux";
+import { StaticRouter } from "react-router-dom/server";
 
 import App from "../src/App.js";
 import Post from "../src/redux/Post.js";
@@ -16,40 +17,54 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static('build', { index: false }));
+if (process.env.PORT) {
+  console.log("PORT EXISTS");
+  function pingWebsite() {
+    https
+      .get("https://recollections-client.onrender.com/", (res) => {
+        console.log("Website pinged successfully");
+      })
+      .on("error", (err) => {
+        console.error("Error while pinging website:", err);
+      });
+  }
+
+  // Ping website every 14 minutes (840000 milliseconds)
+  setInterval(pingWebsite, 840000);
+}
+
+app.use(express.static("build", { index: false }));
 
 app.get("/*", (req, res, next) => {
-    const Store = configureStore({
-      reducer: {
-        post: Post,
-        user: User
-      }
+  const Store = configureStore({
+    reducer: {
+      post: Post,
+      user: User,
+    },
   });
 
   const context = {};
 
   const html = ReactDOMServer.renderToString(
-    <Provider store = { Store }>
+    <Provider store={Store}>
       <StaticRouter location={req.url} context={context}>
         <App />
       </StaticRouter>
     </Provider>
   );
-  
+
   const preloadedState = Store.getState();
 
-  fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf8", (err, data) => {
     if (err) {
       console.error("read file error: ", err);
-      return res.status(500).send('An error occurred');
+      return res.status(500).send("An error occurred");
     }
 
     //return res.status(200);
-    return res.send(data
-                    .replace('<div id="root"></div>',`<div id="root">${html}</div>`)
-                    .replace('__PRELOADED_STATE__', JSON.stringify(preloadedState)));
+    return res.send(data.replace('<div id="root"></div>', `<div id="root">${html}</div>`).replace("__PRELOADED_STATE__", JSON.stringify(preloadedState)));
   });
-})
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`SSR running on port ${PORT}`);
